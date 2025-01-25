0dfbad646730a65de9d4a41d277f20ae796433e9
oovdev <oovdev@gmail.com>
2025-01-25 10:20:59 +0900

    example: ReentrantLock for Virtural thread

--- src/main/java/com/oovdev/purejava/Pinning.java
+++ src/main/java/com/oovdev/purejava/Pinning.java
@@ -5,30 +5,47 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Pinned Virtual thread Case
+ * and
+ * Using ReentrantLock
  * */
 
 @Slf4j
 public class Pinning {
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     // -Djdk.tracePinnedThreads=full or -Djdk.tracePinnedThreads=short 통해 detect
     private final Runnable runnable = new Runnable() {
         @Override
         public void run() {
             log.info("1) run. thread: " + Thread.currentThread());
-            synchronized (this) {   // point of pinned-virtual-thread
-                try {
-                    Thread.sleep(5000);
-
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+//            synchronized (this) {   // point of pinned-virtual-thread
+            lock.lock();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
             }
+//            }
             log.info("2) run. thread: " + Thread.currentThread());
         }
     };
+
+    /**
+     * [synchronized block - about 10s] : Useless case.
+     * 10:15:50.862 [main] INFO com.oovdev.purejava.Pinning -- 2) main. time: 10039, thread: Thread[#1,main,5,main]
+     *
+     *  versus.
+     *
+     * [ReentrantLock - about 5s] : Virtual thread having Same performance with Platform Thread.
+     * 10:14:08.708 [main] INFO com.oovdev.purejava.Pinning -- 2) main. time: 5029, thread: Thread[#1,main,5,main]
+     * */
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
